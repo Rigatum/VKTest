83b2bd8be8bf641e1068e83d3163418cbb69f990
Tagir Melnikov <Melnikov@gmail.ru>
2023-05-05 10:06:38 +0700

    add UserState model

---
+++ src/Models/UserState.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace src.Models
+{
+    public enum CodeState
+    {
+        Active,
+        Blocked
+    }
+    public class UserState
+    {
+        public Guid ID { get; set; }
+        public CodeState Code { get; set; }
+        public string Description { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+
+}
